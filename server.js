17628c4a5fb4a34b4a3d6fc94234add977ef72fc
Glitch (request-header-now) <none>
2018-04-21 12:58:45 +0000

    🐜⛷ Checkpoint ./views/index.html:903892/573 ./server.js:903892/262

--- server.js
+++ server.js
@@ -4,16 +4,23 @@
 // init project
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
 
+// parse application/json
+app.use(bodyParser.json())
+
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
+
+
+
 // http://expressjs.com/en/starter/basic-routing.html
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get("/client", function (request, response) {
+   
 });
 
 // listen for requests :)
